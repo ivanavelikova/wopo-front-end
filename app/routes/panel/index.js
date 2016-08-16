e305408ebe558da7bae5cbab307ceb660d52beeb
Ivana Velikova <Ivana Velikova>
2016-08-16 18:54:55 +0300

    Styling mobile panel

--- app/routes/panel/index.js
+++ app/routes/panel/index.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel: function () {
+    if (window.innerWidth >= 768) {
+      this.transitionTo('panel.statistics');
+    }
+  }
 });
