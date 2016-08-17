04f1a2c11391fb7a57df6933ba5da25a89452710
Mertin Dervish <mertindervish@gmail.com>
2016-08-18 00:07:19 +0300

    Made tether.js available only if it's running in browser

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -21,9 +21,9 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import(app.bowerDirectory + '/tether/dist/js/tether.min.js');
 
-  if ((process.env.EMBER_CLI_FASTBOOT !== 'true')) {
+  if (!process.env.EMBER_CLI_FASTBOOT) {
+    app.import(app.bowerDirectory + '/tether/dist/js/tether.min.js');
     app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.min.js');
   }
 
