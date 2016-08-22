e53db2a9223be01b9c5a1551cb674e104a648d85
Ivana Velikova <Ivana Velikova>
2016-08-22 22:46:12 +0300

    Styling text editor

--- app/components/text-editor.js
+++ app/components/text-editor.js
@@ -1,15 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  didInsertElement() {
+  didInsertElement () {
     const editor = this.$('.summernote');
-    editor.summernote();
-    editor({
+    editor.summernote({
+      height: 200,
       popover: {
         image: [],
         link: [],
         air: []
-      }
+      },
+      fontNames: ['Arial', 'Helvetica', 'Roboto', 'Tahoma', 'Times New Roman', 'Verdana']
     });
+
+    this.set('editor', editor);
+  },
+
+  willDestroyElement () {
+    this.get('editor').summernote('destroy');
   }
 });
