44f12528aa070a6deefaf06a79f3114cf194e51d
Mertin Dervish <mertindervish@gmail.com>
2016-08-30 15:12:29 +0300

    Developed basic registration

--- app/controllers/registration.js
+++ app/controllers/registration.js
@@ -3,7 +3,15 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     register() {
-      alert('Registration');
+      var registerAction = this;
+
+      var user = this.get('store').createRecord('user', {
+        name: registerAction.get('name'),
+        email: registerAction.get('email'),
+        password: registerAction.get('password')
+      });
+
+      user.save();
     }
   }
 });
