36caf43e450369ab4a2fee6c9aaefbda024db0eb
Mertin Dervish <mertindervish@gmail.com>
2016-08-31 20:21:32 +0300

    Added validation translations

--- app/validations/registration.js
+++ app/validations/registration.js
@@ -1,18 +1,29 @@
 import { validator, buildValidations } from 'ember-cp-validations';
 
 const Validations = buildValidations({
-  name: validator('presence', true),
+  name: [
+    validator('presence', true),
+    validator('length', {
+      min: 2,
+      max: 128
+    })
+  ],
   email: [
     validator('presence', true),
-    validator('format', { type: 'email' })
+    validator('format', {
+      regex: /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+      messageKey: 'errors.email'
+    })
   ],
   password: [
     validator('presence', true),
     validator('length', {
       min: 8,
-      max: 48
+      max: 128
     })
   ]
+}, {
+  debounce: 500
 });
 
 export default Validations;
