0105ed5ce450189ac3fd24c5eff0a9b6a2a707fd
Ivana Velikova <Ivana Velikova>
2016-09-04 23:33:09 +0300

    Redesigned and restuctured

--- app/router.js
+++ app/router.js
@@ -6,31 +6,31 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('search');
   this.route('settings');
   this.route('notifications');
   this.route('statistics');
   this.route('articles');
   this.route('portfolio');
-  this.route('panel', function() {
-    this.route('statistics');
+  this.route('registration');
+  this.route('login');
+  this.route('forgot-password');
+  this.route('firststeps');
+
+  this.route('content', function() {
+    this.route('information');
     this.route('articles', function() {
-      this.route('new');
-    });
-    this.route('projects', function() {
       this.route('new');
     });
     this.route('jobs', function() {
       this.route('new');
     });
-    this.route('theme', function() {
-      this.route('store');
+    this.route('projects', function() {
+      this.route('new');
     });
   });
-  this.route('registration');
-  this.route('login');
-  this.route('forgot-password');
-  this.route('firststeps');
+  this.route('theme', function() {
+    this.route('store');
+  });
 });
 
 export default Router;
--- app/routes/panel/index.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  beforeModel: function () {
-    if (window.innerWidth >= 768) {
-      this.transitionTo('panel.statistics');
-    }
-  }
-});
