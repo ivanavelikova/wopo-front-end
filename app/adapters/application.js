03c65bbbff7d407f026bb572ed47e87a1615caba
Mertin Dervish <mertindervish@gmail.com>
2016-09-05 12:23:35 +0300

    Added host in adapter for development purposes

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -6,6 +6,7 @@ const { inject: { service } } = Ember;
 export default DS.RESTAdapter.extend({
   cookies: service(),
 
+  host: 'http://localhost:3333',
   headers: Ember.computed(function() {
     const csrfToken = this.get('cookies').read('XSRF-TOKEN');
     return {
