1314e399dd2f1bf99e9b5bc86289df6f9230f4c0
Mertin Dervish <mertindervish@gmail.com>
2016-09-07 15:23:31 +0300

    Added auth-form component to registration

--- app/controllers/registration.js
+++ app/controllers/registration.js
@@ -4,54 +4,29 @@ export default Ember.Controller.extend({
   intl: Ember.inject.service(),
 
   disableForm: false,
-  alerts: {
-    success: null,
-    danger: null
+  alert: {
+    type: null,
+    content: null
   },
-
-  showSuccessAlert: Ember.computed('alerts.success', function() {
-    return (this.get('alerts.success') !== null);
-  }),
-  showDangerAlert: Ember.computed('alerts.danger', function() {
-    return (this.get('alerts.danger') !== null);
-  }),
 
   actions: {
     register() {
-      const intl = this.get('intl');
+      const registerAction = this;
 
-      // ------
-      // Validation fail
-      // ------
-
-      if (!this.get('model').get('validations.isValid')) {
-        $('html, body').animate({ scrollTop: 0 });
-
-        this.set('alerts.success', null);
-        this.set('alerts.danger', intl.t('errors.fill'));
-        return;
-      }
-
-      // ------
-      // Validation success
-      // ------
-
-      this.set('disableForm', true);
-      this.set('alerts.success', null);
-      this.set('alerts.danger', null);
-
-      var registerAction = this;
-      var user = this.get('store').createRecord('user', {
+      const user = this.get('store').createRecord('user', {
         name: registerAction.get('name'),
         email: registerAction.get('email'),
         password: registerAction.get('password')
       });
 
-      function showSuccessAlert() {
-        registerAction.set('disableForm', false);
-        $('html, body').animate({ scrollTop: 0 });
+      user.save().then(success, failure).catch(failure);
 
-        registerAction.set('alerts.success', intl.t('success.registration'));
+      function success () {
+        registerAction.set('disableForm', false);
+        registerAction.set('alert', {
+          type: 'success',
+          content: registerAction.get('intl').t('success.registration')
+        });
 
         $('input').blur();
         registerAction.set('name', null);
@@ -60,21 +35,19 @@ export default Ember.Controller.extend({
         registerAction.set('didValidate', false);
       }
 
-      function failure(reason) {
+      function failure (reason) {
         registerAction.set('disableForm', false);
-        $('html, body').animate({ scrollTop: 0 });
+        let alertContent = registerAction.get('intl').t('errors.serverFail');
 
-        registerAction.set('alerts.success', null);
-        
-        if (!reason.errors || !reason.errors[0].detail) {
-          registerAction.set('alerts.danger', intl.t('errors.serverFail'));
-          return;
+        if (reason.errors[0].detail) {
+          alertContent = reason.errors[0].detail;
         }
 
-        registerAction.set('alerts.danger', reason.errors[0].detail);
+        registerAction.set('alert', {
+          type: 'danger',
+          content: alertContent
+        });
       }
-
-      user.save().then(showSuccessAlert, failure).catch(failure);
     }
   }
 });
