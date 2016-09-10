84d87f38bd62323cf58a4e1a278e48270e69fc86
Mertin Dervish <mertindervish@gmail.com>
2016-09-10 23:33:10 +0300

    Added restore method to jwt authenticator

--- app/authenticators/jwt.js
+++ app/authenticators/jwt.js
@@ -1,13 +1,30 @@
 import Ember from 'ember';
 import Base from 'ember-simple-auth/authenticators/base';
 
+const {
+  RSVP,
+  isEmpty
+} = Ember;
+
 export default Base.extend({
   restore(data) {
+    return new RSVP.Promise((resolve, reject) => {
+      if (!this._validate(data)) {
+        reject();
+        return;
+      }
+
+      resolve(data);
+    });
   },
 
   authenticate(/*args*/) {
   },
 
-  invalidate(data) {
+  invalidate(/*data*/) {
+  },
+
+  _validate(data) {
+    return !isEmpty(data['token']);
   }
 });
