b41e5aa32b89b96d1265e219000f3f21057071b8
Mertin Dervish <mertindervish@gmail.com>
2016-09-10 23:30:38 +0300

    Developed JWT authorizer

---
+++ app/authorizers/jwt.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+import Base from 'ember-simple-auth/authorizers/base';
+
+const { isEmpty } = Ember;
+
+export default Base.extend({
+  authorize(data, block) {
+    const token = data['token'];
+
+    if (!isEmpty(token)) {
+      block('Authorization', `Bearer ${token}`);
+    }
+  }
+});
