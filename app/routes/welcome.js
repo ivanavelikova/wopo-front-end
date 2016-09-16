47298b9e1460ab6287d914af29daaed0ed12b160
Mertin Dervish <mertindervish@gmail.com>
2016-09-16 10:51:11 +0300

    Made /dashboard/* and /welcome route only available for authenticated users

--- app/routes/welcome.js
+++ app/routes/welcome.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+const routeMixin = Ember.Mixin.create(AuthenticatedRouteMixin);
+
+export default Ember.Route.extend(routeMixin, {
 });
