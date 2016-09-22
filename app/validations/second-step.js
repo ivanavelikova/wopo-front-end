6032bd11ab36911c37846857b892bef07ab2f827
Mertin Dervish <mertindervish@gmail.com>
2016-09-22 13:16:40 +0300

    Added validation for skills in second-step

--- app/validations/second-step.js
+++ app/validations/second-step.js
@@ -3,6 +3,9 @@ import { validator, buildValidations } from 'ember-cp-validations';
 const Validations = buildValidations({
   'data.about': [
     validator('presence', true)
+  ],
+  'data.skills': [
+    validator('presence', true)
   ]
 });
 
