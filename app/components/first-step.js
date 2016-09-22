70f5cf5e31621a1833985d7f97a7d65e86549cec
Mertin Dervish <mertindervish@gmail.com>
2016-09-22 20:30:43 +0300

    Fixed card-title with button

--- app/components/first-step.js
+++ app/components/first-step.js
@@ -4,6 +4,20 @@ import Validations from '../validations/first-step';
 const { computed } = Ember;
 
 export default Ember.Component.extend(Validations, {
+  alert: {
+    type: null,
+    content: null
+  },
+
+  showAlert: Ember.computed('alert.{type,content}', function() {
+    if (this.get('alert.type') !== null && this.get('alert.content') !== null) {
+      $('html, body').animate({ scrollTop: 0 });
+      return true;
+    }
+
+    return false;
+  }),
+  
   isFirstTheme: computed('data.themeId', function () {
     if (this.get('data.themeId') === 1) {
       return true;
