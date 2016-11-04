92206bf18d967f9b0f3ccb9ee3af91c417a79682
Ivana Velikova <Ivana Velikova>
2016-11-04 10:52:50 +0200

    Started reviews' section

--- tests/integration/components/person-managing-test.js
+++ tests/integration/components/person-managing-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('full-control', 'Integration | Component | full control', {
+moduleForComponent('person-managing', 'Integration | Component | person managing', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{full-control}}`);
+  this.render(hbs`{{person-managing}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#full-control}}
+    {{#person-managing}}
       template block text
-    {{/full-control}}
+    {{/person-managing}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
